b004e58683c253037960551c4eb6cfe71c1e5dd2
Rashicyber <rashidzafarb2b@proton.me>
2024-07-31 05:53:30 -0700

    Update test_solver.py

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,5 +1,9 @@
 # test_solver.py
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from solver import decrypt, create_key_from_mapping
 
